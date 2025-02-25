db1dc07a0309af9d87931f29dd3bca7cf8a7a63a
jitenderkumar2030 <jobs.jitenderkr@gmail.com>
2025-02-25 18:05:47 +0530

    code update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    log: ['error'],
+    errorFormat: 'minimal',
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    }
+  })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
